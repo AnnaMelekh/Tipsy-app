a0f9c77cd488b139a07d0b54581767a3620c0427
AnnaMelekh <annasmelekhina@gmail.com>
2024-10-09 14:01:41 +0200

    created oulets and actions

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPCTButton: UIButton!
+    
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+//        zeroPctButton.isSelected = true
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: Any) {
+    }
+    
+}
+
---
+++ Tipsy/Controllers/ResulttViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResulttViewController.swift
+//  Tipsy
+//
+//  Created by Anna Melekhina on 09.10.2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResulttViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var settingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func recalculateButton(_ sender: Any) {
+    }
+}
